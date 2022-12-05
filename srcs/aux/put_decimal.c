924f2778e5b011d5062e7204060d8fd8d5ae590e
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2022-12-05 18:43:52 -0300

    this makefile is also f*cking perfect.

--- srcs/aux/put_decimal.c
+++ srcs/aux/put_decimal.c
@@ -6,13 +6,11 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022 23:32:49 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/11/28 20:42:46 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/12/05 18:38:06 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-//anger is an energy
 
 int	put_decimal(signed long value)
 {
